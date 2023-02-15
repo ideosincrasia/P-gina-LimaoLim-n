49c062116c473d9a4e313f955d12a3c66a2681e8
ideosincrasia <125385828+ideosincrasia@users.noreply.github.com>
2023-02-15 10:10:14 +0100

    Add files via upload

---
+++ Imagen Sprite/Solución Js/swap.js
@@ -0,0 +1,26 @@
+var vientoactual;
+
+function vientoid0() {
+    vientoactual = 0;
+    document.getElementById('vientovar').style.setProperty("--viento", vientoactual);
+};
+
+function vientoid1() {
+    vientoactual = 1;
+    document.getElementById('vientovar').style.setProperty("--viento", vientoactual);
+};
+
+function vientoid2() {
+    vientoactual = 2;
+    document.getElementById('vientovar').style.setProperty("--viento", vientoactual);
+};
+
+function vientoid3() {
+    vientoactual = 3;
+    document.getElementById('vientovar').style.setProperty("--viento", vientoactual);
+};
+
+function vientoid4() {
+    vientoactual = 4;
+    document.getElementById('vientovar').style.setProperty("--viento", vientoactual);
+};
